a7630c158c7b29a66b4acde8cfb6ad5541ef5db2
BaldTheBuilder <44854267+anderwhite@users.noreply.github.com>
2022-03-31 12:36:28 -0400

    commit

--- leaflet/Leaflet.r
+++ leaflet/Leaflet.r
@@ -1,13 +1,22 @@
-# This documnt is created in the lanuage R.
+# This code is designed to create an interactive Leaflet map. 
+# This document is created in the language R.
+
 library(leaflet)
+library(htmlwidgets)
 library(foreign)
 library(sf)
 library(rgdal)
-setwd("C:\batcave\GG469\layers")
+
+setwd("C:/batcave/GG469/layers")
+
 dsn <- "Leaflet_Features_WGS84.shp"
 cb5=readOGR(dsn)
-final_test <- st_read("C:/Users/down3170/OneDrive - wilfrid Laurier University/Leaflet_Features_WGS84/Leaflet_Features_WGS84.shp")
+
+final_test <- st_read("C:/batcave/GG469/layers/Leaflet_Features_WGS84.shp")
+
 m <- leaflet(final_test) %>% addPolygons(color="#444444", weight= 1, smoothFactor= 0.5, opacity=1.0) %>%
-  addMarkers(lng=-79.772886, lat=43.721614, label="core")%>%
-  addMarkers(lng=-79.744383, lat=43.73305, label="corridor") %>%
-  addMarkers(lng=-79.778544, lat=43.761336, label="stepping stone")
+    addMarkers(lng=-79.772886, lat=43.721614, label="core")%>%
+    addMarkers(lng=-79.744383, lat=43.73305, label="corridor") %>%
+    addMarkers(lng=-79.778544, lat=43.761336, label="stepping stone")
+
+saveWidget(m, file="map1.html")
